84d944ac01ede0d2faedd25d123a817cbfb59652
Jesse Roberts <jesse@jesseops.net>
2022-06-25 00:07:31 +0000

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mkdocs-unity-plugin',
-    version='0.2.0',
+    version='0.3.0',
     description='An MkDocs plugin that unifies multiple MkDocs repositories',
     long_description='',
     keywords='mkdocs python markdown wiki',
